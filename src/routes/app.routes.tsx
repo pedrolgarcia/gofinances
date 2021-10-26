ccf4a826dc493fec9a173d5b10d778f1001ccb70
Pedro Lopes <pedrolopesgrc@gmail.com>
2021-10-25 22:48:24 -0300

    feat: resume page

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -8,6 +8,7 @@ import { MaterialIcons } from "@expo/vector-icons";
 
 import Dashboard from "../screens/Dashboard";
 import Register from "../screens/Register";
+import Resume from "../screens/Resume";
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
@@ -61,7 +62,7 @@ function AppRoutes() {
       />
       <Screen
         name="Resumo"
-        component={Register}
+        component={Resume}
         options={{
           tabBarIcon: ({ size, color }) => (
             <MaterialIcons name="pie-chart" size={size} color={color} />
