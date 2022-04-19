11911fc35100af945744a88404d6bf531ccd1661
Pedro Lopes <pedrolopesgrc@gmail.com>
2022-04-19 00:48:28 -0300

    feat: hook test and mocks

--- src/hooks/auth.spec.tsx
+++ src/hooks/auth.spec.tsx
@@ -1,15 +1,40 @@
-import React from "react";
-import { render } from "@testing-library/react-native";
-import { renderHook } from "@testing-library/react-hooks";
+import 'jest-fetch-mock'
+
+import { act, renderHook } from "@testing-library/react-hooks";
+import fetchMock from 'jest-fetch-mock';
 
 import { AuthProvider, useAuth } from "./auth";
 
+fetchMock.enableMocks();
+
+const userTest = {
+  id: 'any_id',
+  email: 'john.doe@email.com',
+  name: 'John Doe',
+  photo: 'any_photo.png'
+};
+
+jest.mock('expo-auth-session', () => {
+  return {
+    startAsync: () => ({
+      type: 'success',
+      params: {
+        access_token: 'any_token',
+      }
+    }),
+  }
+})
+
 describe("Auth Hook", () => {
-  it("should be able to sign in with existing Google account", () => {
+  it("should be able to sign in with existing Google account", async () => {
+    fetchMock.mockResponseOnce(JSON.stringify(userTest));
+
     const { result } = renderHook(() => useAuth(), {
       wrapper: AuthProvider
     });
 
-    
+    await act(() => result.current.signInWithGoogle())
+
+    expect(result.current.user.email).toBe(userTest.email);
   });
 });
