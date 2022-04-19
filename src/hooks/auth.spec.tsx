8d830db72c18626e3da1fc44e571413b07d14f78
Pedro Lopes <pedrolopesgrc@gmail.com>
2022-04-18 23:45:36 -0300

    feat: renderHook

--- src/hooks/auth.spec.tsx
+++ src/hooks/auth.spec.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { render } from "@testing-library/react-native";
+import { renderHook } from "@testing-library/react-hooks";
 
 import { AuthProvider, useAuth } from "./auth";
 
 describe("Auth Hook", () => {
   it("should be able to sign in with existing Google account", () => {
-    const {} = useAuth();
+    const { result } = renderHook(() => useAuth(), {
+      wrapper: AuthProvider
+    });
+
+    
   });
 });
